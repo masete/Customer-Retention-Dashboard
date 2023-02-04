ffe8254d42c56d4b4d76cff573a5a54a4eff8422
masete <nicholasmasete72@gmail.com>
2023-02-04 17:15:12 +0300

    importations

---
+++ CRetention.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import matplotlib as mpl 
+from datetime import date, datetime
+import streamlit as st  
